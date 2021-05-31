ad2b70ebf51a61a321454a99a0a32a0ef9ce42f1
BrandonReno <b_reno@u.pacific.edu>
2021-05-31 11:04:01 -0700

    adding additional logging as well as documentation to delete

--- controllers/delete.go
+++ controllers/delete.go
@@ -19,28 +19,36 @@ func (l * Aer_Log) DeleteWorkout(rw http.ResponseWriter, r *http.Request){
 	athlete_id := getAthleteID(r)
 	workout_id, err := getWorkoutID(r)
 	if err != nil{
+		l.l.Printf("Error: Unable to convert workout id to string: %s", err)
 		http.Error(rw, fmt.Sprintf("Error getting workout ID: %s", err), http.StatusBadRequest)
 		return
 	}
 
 	err = l.db.DeleteWorkout(athlete_id, workout_id)
 	if err != nil{
+		l.l.Printf("Error: Unable to delete athlete, can not find either workout id or athlete id: %s", err)
 		http.Error(rw, fmt.Sprintf("Error in deleting workout: %s", err), http.StatusBadRequest)
 		return
 	}
 }
 
-
+//Delete an athlete from the system
 func (l * Aer_Log) DeleteAthlete(rw http.ResponseWriter, r *http.Request){
-
-
-	//swagger
+	// swagger:route DELETE /athletes/athlete{id} athletes deleteAthlete
+    //
+    // Deletes a specified athlete by id
+    //
+    //      Schemes: http
+    //		
+	//		Responses:
+	//			201: noContent
+	//			404: badRequest
 
 	athlete_id := getAthleteID(r)
 	err := l.db.DeleteAthlete(athlete_id)
 	if err != nil{
+		l.l.Printf("Error: Could not delete athlete, athlete id may be incorrect: %s", err)
 		http.Error(rw, fmt.Sprintf("Error deleting athlete: %s", err), http.StatusBadRequest)
 		return
 	}
-	
 }
