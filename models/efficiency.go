338b72a8a8bb7701e7adf5ca983ffd7de2401012
BrandonReno <b_reno@u.pacific.edu>
2021-06-11 15:23:49 -0700

    Efficiency updates, beginning to integrate getting athlete efficiency into system

--- models/efficiency.go
+++ models/efficiency.go
@@ -7,5 +7,37 @@ type Efficiency struct {
 	Athlete_ID       string  `json:"-"`
 }
 
+func (e *Efficiency) CalculateEfficiency(w []Workout){
+	averageRating := 0
 
+	for _, workout := range w{
+		averageRating += workout.Rating
+	}
+
+	averageRating = averageRating/len(w)
+
+	if w[0].Rating < averageRating{
+		e.Efficiency_Score /= float64(w[0].Rating)
+	} else{
+		e.Efficiency_Score *= float64(w[0].Rating)
+	}
+}
+
+func (e *Efficiency) CalculateSport(w []Workout){
+	sportMap := make(map[string]int)
+
+	for _, workout := range w{
+		sportMap[workout.Sport] += 1
+	}
+
+	favoriteSport := ""
+	maximum := 0
+	for key,val := range sportMap{
+		if val > maximum{
+			favoriteSport = key
+		}
+	}
+
+	e.Favorite_Sport = favoriteSport
+}
 
--- services/efficiency.go
+++ services/efficiency.go
@@ -4,7 +4,7 @@ import(
 	"github.com/BrandonReno/A.E.R/models"
 )
 
-func(db *DB) CalculateEfficiency(a *models.Athlete) (models.Efficiency, error){
+func(db *DB) GetEfficiency(a *models.Athlete) (models.Efficiency, error){
 	athlete_id := a.Athlete_ID
 	sqlStatement := `SELECT * FROM public.efficiency WHERE Athlete_ID = $1`
 	row := db.DBConn.QueryRow(sqlStatement, athlete_id)
